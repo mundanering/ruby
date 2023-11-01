ea9379dceb6c4ddf04dab4df33a47876e9c84a9d
mundanering <mundanering@gmail.com>
2023-11-01 19:48:57 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import math
 import os
+import random
 import random as rn
 
 import chess
@@ -10,12 +11,14 @@ import torch.optim
 from torch import nn
 from torch.nn.utils.rnn import pad_sequence
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 SIDES = ['white', 'black']
 
 SAVE_HISTORY = False
-EPOCHS = 10000
+GAMES = 100000000
 H_DIM = 512
-L_DIM = 1
+L_DIM = 4
 
 
 class RubyCore(nn.Module):
@@ -27,6 +30,12 @@ class RubyCore(nn.Module):
         self.lstm = nn.LSTM(input_dim, hidden_dim, layers_dim)
         self.fc = nn.Linear(hidden_dim, output_dim)
 
+        for name, param in self.lstm.named_parameters():
+            if 'weight_ih' in name:
+                nn.init.xavier_uniform_(param.data)
+            elif 'weight_hh' in name:
+                nn.init.xavier_uniform_(param.data)
+
     def forward(self, x):
         out, _ = self.lstm(x)
         out = self.fc(out[:, -1, :])
@@ -37,6 +46,7 @@ def get_legal_moves(_board):
     legal_moves = []
     for moves in _board.legal_moves:
         legal_moves.append(moves)
+    random.shuffle(legal_moves)
     return legal_moves
 
 
@@ -60,39 +70,79 @@ def create_inputs(_ll, _history, _board, _player):
 
 
 def get_output(_model, _inputs, _ll):
+    _model = _model.to(device)
+    _inputs = _inputs.to(device)
     output = _model(_inputs)
 
     res = output.item()
-    index = math.floor((res * 100))
+    # print('res: ', res)
+    index = math.floor(res * 1e8)
     index = index % len(_ll)
 
     return index
 
 
 def game_state(_board):
-    info = _board.outcome()
-    return info.winner
+    if _board.is_checkmate():
+        if _board.turn:
+            return 'black'
+        else:
+            return 'white'
 
 
-def calculate_loss(_win):
-    if _win:
-        return -1.00
-    else:
-        return 10.00
+def calculate_loss(_win, _board, colour):
+    if _win == 'yes':
+        if colour == 'white':
+            if 'Q' in _board.board_fen():
+                return 1.00
+            else:
+                if _board.fullmove_number <= 20:
+                    return -1.00
+                else:
+                    return 0.01
+        else:
+            if 'q' in _board.board_fen():
+                return 1.00
+            else:
+                if _board.fullmove_number <= 20:
+                    return -1.00
+                else:
+                    return 0.01
+    elif _win == 'no':
+        if colour == 'white':
+            if 'Q' in _board.board_fen():
+                return 10.00
+            else:
+                return 7.00
+        else:
+            if 'q' in _board.board_fen():
+                return 10.00
+            else:
+                return 7.00
+    elif _win == 'draw':
+        if colour == 'white':
+            if 'Q' in _board.board_fen():
+                return 7.00
+            else:
+                return 5.00
+        else:
+            if 'q' in _board.board_fen():
+                return 7.00
+            else:
+                return 5.00
 
 
 def train(_model, _optimiser):
+    board = chess.Board()
     if os.path.isfile('model.pth'):
         _model = torch.load('model.pth')
-    for epoch in range(EPOCHS):
+    for game in range(GAMES):
         opponent = RubyCore()
         opponent.load_state_dict(_model.state_dict())
 
         p1, p2 = rn.sample(SIDES, 2)
 
         history = []
-
-        board = chess.Board()
 
         while True:
             if p1 == 'white':
@@ -152,26 +202,34 @@ def train(_model, _optimiser):
             with open('match_history.json', 'w') as f:
                 json.dump(existing_data, f, indent=4)
 
+        print(board)
+        print(history)
+        print(p1, winner)
         if p1 == winner:
-            l_val = calculate_loss(True)
+            l_val = calculate_loss('yes', board, p1)
             print('win')
-        else:
-            l_val = calculate_loss(False)
+        elif p2 == winner:
+            l_val = calculate_loss('no', board, p1)
             print('loss')
+        else:
+            l_val = calculate_loss('draw', board, p1)
         loss = torch.tensor([l_val], requires_grad=True)
+
+        print('training loss:', l_val)
 
         _optimiser.zero_grad()
         loss.backward()
         _optimiser.step()
+        board.reset()
 
-        if (epoch + 1) % 10 == 0:
-            print('Epoch [{}/{}]'.format(epoch + 1, EPOCHS))
+        if (game + 1) % 10 == 0:
+            print('Games [{}/{}]'.format(game + 1, GAMES))
             torch.save(_model, 'model.pth')
 
 
 if __name__ == '__main__':
     model = RubyCore()
 
-    optimiser = torch.optim.Adam(model.parameters(), lr=1e-4)
+    optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
 
     train(model, optimiser)
